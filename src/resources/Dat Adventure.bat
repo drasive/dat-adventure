1c2137f934f5c3723b827780e077bfc17f1e5c50
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-08-22 14:14:07 +0200

    Extended the windows executable

--- src/resources/Dat Adventure.bat
+++ src/resources/Dat Adventure.bat
@@ -1,1 +1,15 @@
+@echo off
+
+REM Set the JAVA_HOME
+for /f %%j in ("java.exe") do (
+    set JAVA_HOME=%%~dp$PATH:j
+)
+
+REM Execute the application
 java -jar "Dat Adventure.jar"
+
+REM Check for errors
+if NOT ["%errorlevel%"]==["0"] (
+    pause
+    exit /b %errorlevel%
+)
